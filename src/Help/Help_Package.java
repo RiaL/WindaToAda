5191eac6201dfbfb6b3b118dd6491ea04ec51c24
kkutt <krzysztof.kutt@gmail.com>
2013-02-17 18:44:56 +0100

    projekt w netbeansie (teraz bez ady)

---
+++ src/Help/Help_Package.java
@@ -0,0 +1,12 @@
+package Help;
+
+/**
+ * Tymczasowy zastepnik klasy, ktora bedzie wygenerowana z Ady!
+ */
+public class Help_Package {
+    
+    public static int Sum(int liczba1, int liczba2) {
+        return liczba1+liczba2;
+    }
+    
+}
---
+++ src/ada/Passenger.java
@@ -0,0 +1,17 @@
+package ada;
+
+/**
+ *
+ * @author Krzysztof Kutt & Michal Nowak
+ */
+public class Passenger {
+    int destination;
+
+    public Passenger(int destination) {
+        this.destination = destination;
+    }
+
+    public int getDestination() {
+        return destination;
+    }
+}
